aaa6db32b1124427ceadbb78aca96564e91cad6e
BuildTools <unconfigured@null.spigotmc.org>
2019-11-13 15:09:16 -0600

    Rewrite now has working Source List, Sink List, and Refresh button

--- gui_logic.py
+++ gui_logic.py
@@ -1,8 +1,11 @@
+from tkinter import ttk
 import program_logic
-import tkinter as tk
+import functools
 import traceback
 import logging
+import tkinter
 import sys
+
 
 logger = logging.getLogger("Main")
 
@@ -17,3 +20,170 @@ def log_exception_handler(type, value, tb):
 
 
 sys.excepthook = log_exception_handler
+
+
+def run_gui():
+    palt_gui = PaltGui()
+    palt_gui.run_gui()
+
+
+class PaltGui:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window_name = "PulseAudio Loopback Tool"
+        self.tab_controller = ttk.Notebook(self.window)
+        self.loopback_tab = LoopbackTab(self.window)
+
+        # self.style = ttk.Style()
+
+    def run_gui(self):
+        # self.window = tkinter.Tk()
+        self.setup_gui(self.window)
+        self.window.mainloop()
+
+    def setup_gui(self, window):
+        # self.style.configure("BW.TLabel", foreground="black", background="white")
+        window.title(self.window_name)
+        window.geometry("500x300")
+        window.columnconfigure(0, weight=1)
+        window.rowconfigure(1, weight=1)
+
+        # tab_master = ttk.Notebook(window)
+        self.tab_controller.grid(column=0, row=1, sticky=tkinter.N + tkinter.S + tkinter.E + tkinter.W)
+        # tab1 = LoopbackTab(window)
+        tab2 = ttk.Frame(self.tab_controller)
+        tab3 = ttk.Frame(self.tab_controller)
+        self.tab_controller.add(self.loopback_tab, text=self.loopback_tab.text_name)
+        self.tab_controller.add(tab2, text="Tab Two")
+        self.tab_controller.add(tab3, text="Tab Three")
+
+        lol = ttk.Label(tab2, text="Hello!")
+        lol.grid(column=0, row=0)
+
+        global_refresh = ttk.Button(window, text="Refresh All", command=self.global_refresh)
+        global_refresh.grid(column=0, row=0)
+
+    def global_refresh(self):
+        logger.info("Global refresh triggered.")
+        source_list = program_logic.reworked_get_source_list()
+        sink_list = program_logic.reworked_get_sink_list()
+        self.loopback_tab.refresh(source_list, sink_list)
+
+
+class TabController:
+    def __init__(self, parent):
+        self.parent = parent
+
+
+class LoopbackTab(ttk.Frame):
+    def __init__(self, parent_notebook, **kwargs):
+        super().__init__(parent_notebook, **kwargs)
+        self.text_name = "Loopback"
+        self.parent = parent_notebook
+
+        self.source_list = SourceSinkList(self, "Source List")
+        self.source_list.grid(column=0, row=0, rowspan=2, sticky=tkinter.NSEW)
+
+        self.source_label = ttk.Label(self, text="Source")
+        self.source_label.grid(column=1, row=0, sticky=tkinter.S)
+        self.source_entry = ttk.Entry(self, width=6)
+        self.source_entry.grid(column=1, row=1, padx=5, pady=5, sticky=tkinter.N)
+
+        self.loopback_label = ttk.Label(self, text="will pipe sound to")
+        self.loopback_label.grid(column=2, row=0, sticky=tkinter.S)
+        self.loopback_button = ttk.Button(self, text="Create Loopback")
+        self.loopback_button.grid(column=2, row=1, padx=5, pady=5, sticky=tkinter.N)
+
+        self.sink_label = ttk.Label(self, text="Sink")
+        self.sink_label.grid(column=3, row=0, sticky=tkinter.S)
+        self.sink_entry = ttk.Entry(self, width=6)
+        self.sink_entry.grid(column=3, row=1, padx=5, pady=5, sticky=tkinter.N)
+
+        self.sink_list = SourceSinkList(self, "Sink List")
+        self.sink_list.grid(column=4, row=0, rowspan=2, sticky=tkinter.NSEW)
+
+        self.configure_weights()
+
+    def configure_weights(self):
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(4, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+
+    def refresh(self, source_list, sink_list):
+        self.source_list.refresh(source_list)
+        self.sink_list.refresh(sink_list)
+
+
+class SourceSinkList(ttk.LabelFrame):
+    def __init__(self, parent, name, **kwargs):
+        super().__init__(parent, text=name, **kwargs)
+
+        self.list_box = self.list_box = tkinter.Listbox(self)
+        self.vertical_scrollbar = ttk.Scrollbar(self, orient="vertical")
+        self.horizontal_scrollbar = ttk.Scrollbar(self, orient="horizontal")
+
+        self.configure_list_box()
+        self.configure_vertical_scrollbar(self.list_box)
+        self.configure_horizontal_scrollbar(self.list_box)
+
+        self.configure_weights()
+
+    def configure_list_box(self):
+        self.list_box = tkinter.Listbox(self)
+        self.list_box.grid(column=0, row=0, sticky=tkinter.NSEW)
+
+    def configure_vertical_scrollbar(self, list_box):
+        self.vertical_scrollbar.config(command=list_box.yview)
+        self.vertical_scrollbar.grid(column=1, row=0, sticky=tkinter.NS + tkinter.E)
+        list_box.config(yscrollcommand=self.vertical_scrollbar.set)
+
+    def configure_horizontal_scrollbar(self, list_box):
+        self.horizontal_scrollbar.config(command=list_box.xview)
+        self.horizontal_scrollbar.grid(column=0, row=1, sticky=tkinter.S + tkinter.EW)
+        list_box.config(xscrollcommand=self.horizontal_scrollbar.set)
+
+    def configure_weights(self):
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+    def refresh(self, item_list):
+        self.list_box.delete(0, tkinter.END)
+        for i in range(len(item_list)):
+            self.list_box.insert(tkinter.END, item_list[i]["nice_name"])
+            self.list_box.itemconfig(i, {"bg": item_list[i]["color"]})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- program_logic.py
+++ program_logic.py
@@ -18,3 +18,165 @@ def log_exception_handler(type, value, tb):
 
 sys.excepthook = log_exception_handler
 
+
+def open_pavucontrol():
+    logger.info("Opening pavucontrol.")
+    subprocess.Popen("pavucontrol", shell=True, stdout=subprocess.PIPE)
+
+
+def list_sinks():
+    output = subprocess.getoutput("pactl list sinks short")
+    return output
+
+
+def list_sources():
+    output = subprocess.getoutput("pactl list sources short")
+    return output
+
+
+def list_modules():
+    output = subprocess.getoutput("pactl list modules short")
+    return output
+
+
+def process_list(inputlist):
+    '''
+    Takes an input list and processes it to make it easier to work with later on. Used with the list_*() functions.
+    :param inputlist: A string that consists of at least 1 line and 1 tab, typically from the list_*() functions.
+    :return: A 2D list, with each sentence being the first dimension and each part of the sentence separated by tabs
+    being the second dimension.
+    '''
+    splitlinelist = inputlist.splitlines()
+    output = []
+    for line in splitlinelist:
+        attachment = []
+        splitline = line.split("\t")
+        for part in splitline:
+            attachment.append(part)
+        output.append(attachment)
+    return output
+
+
+def color_tag(state):
+    '''
+    Used to convert a state to a color then output that color for the devices
+    in listbox_sink_list and listbox_source_list.
+    :param state: A string.
+    :return: A color for use by tkinter.
+    '''
+    # output = "default"
+    if state == "RUNNING":
+        output = "green"
+    elif state == "IDLE":
+        output = "green"
+    elif state == "SUSPENDED":
+        output = "yellow"
+    else:
+        output = "red"
+    return output
+
+
+def process_attribute(input_list, args):
+    '''
+    This function takes a line of different attributes separated with spaces, and outputs the ones specified in args
+    in the order given.
+    :param input_list: A string containing a list of attributes separated with spaces.
+    :param args: String(s) containing the name of the attribute you are looking for. Multiple strings can be given.
+    :return: A list of strings containing the attribute and its value.
+    '''
+    output = []
+    attribute_list = input_list.split(" ")
+    for attribute in attribute_list:
+        for argument in args:
+            if attribute[:len(argument)] == argument:
+                output.append(attribute)
+
+    return output
+
+
+def process_module(input_module, *args):
+    '''
+    This function takes a list of strings that correspond to a single line from "pactl list modules short" and what
+    attributes you want listed and returns a proper string name for displaying.
+    :param input_module: List of strings containing information about the module in the following order:
+    id, module name, (optional) attributes.
+    :param args: String(s) containing the name of the attribute you are looking for. Multiple strings can be given.
+    :return: Formatted string for displaying in a listbox.
+    '''
+    output = input_module[0] + "   " + input_module[1]
+    if len(input_module) > 2:
+        attribute_list = process_attribute(input_module[2], args)
+        for attribute in attribute_list:
+            output += "   " + attribute
+    return output
+
+
+def process_module_list(input_list):
+    '''
+    This function is passed a string input from "pactl list modules short" and organizes it as needed.
+    :param input_list: A string with at least 1 line, and at least 2 different items separated by tabs.
+    :return: A list of shortened names of applicable modules.
+    '''
+    processed_list = process_list(input_list)
+    devices = []
+    for item in processed_list:
+        if len(item) < 2:
+            # log(WARNING, "process_module_list", "Module with less than 2 items found: \"" + str(item) + "\"")
+            logger.warning("Module with less than 2 items found: {}".format(item))
+        # If the item is some sort of null sink (because it could be made via this program):
+        elif item[1] == "module-null-sink":
+            temp = process_module(item, "sink_name")
+            devices.append(temp)
+        # If the item is some sort of loopback (because it could be made via this program):
+        elif item[1] == "module-loopback":
+            temp = process_module(item, "sink", "source")
+            devices.append(temp)
+        # If the item is some sort of null source (because it could be made via this program):
+        elif item[1] == "module-null-source":
+            temp = process_module(item, "source_name")
+            devices.append(temp)
+        # If the item is some sort of remapped source (because it could be made via this program):
+        elif item[1] == "module-remap-source":
+            temp = process_module(item, "source_name", "master")
+            devices.append(temp)
+        else:
+            # If its not any of the modules listed above, this program doesnt care about it.
+            pass
+    return devices
+
+
+"""
+import subprocess
+output = subprocess.getoutput("pactl list sinks short")
+"""
+
+
+def reworked_process_short_audio_list(raw_list_string: str) -> list:
+    # Made specifically for processing the short sink and source lists.
+    list_of_strings = raw_list_string.split("\n")
+    device_list = list()
+    for string in list_of_strings:
+        # raw_device_list.append(string.split("\t"))
+        device_list.append(short_audio_listing_to_dict(string))
+    return device_list
+
+
+def short_audio_listing_to_dict(raw_listing_string: str) -> dict:
+    raw_listing = raw_listing_string.split("\t")
+    if len(raw_listing) == 5:
+        nice_name = "{} {} {}".format(raw_listing[0], raw_listing[1], raw_listing[4])
+        return {"id": raw_listing[0], "name": raw_listing[1], "driver": raw_listing[2], "spec": raw_listing[3],
+                "state": raw_listing[4], "color": color_tag(raw_listing[4]), "nice_name": nice_name}
+    else:
+        logger.warning("The given short audio listing doesn't appear to be right: {}".format(raw_listing_string))
+        return {"id": "BAD", "name": "BAD", "driver": "BAD", "spec": "BAD", "state": "BAD", "color": color_tag("BAD"),
+                "nice_name": "BAD BAD BAD"}
+
+
+def reworked_get_source_list():
+    return reworked_process_short_audio_list(list_sources())
+
+
+def reworked_get_sink_list():
+    return reworked_process_short_audio_list(list_sinks())
+
