75043ce0346910d67f71f80e0c6026a6627324db
BuildTools <unconfigured@null.spigotmc.org>
2019-11-13 15:08:41 -0600

    Moved things around

--- start.py
+++ start.py
@@ -12,13 +12,13 @@ FORMAT = "[{asctime}][{filename}][{lineno:3}][{funcName}][{levelname}] {message}
 LOGGING_LEVEL = logging.DEBUG
 
 
-def log_exception_handler(type, value, tb):
+def log_exception_handler(error_type, value, tb):
     # TODO: Unify logging errors.
     the_logger = logging.getLogger("Main")
     the_logger.critical("Uncaught exception:\n"
                         "Type: {}\n"
                         "Value: {}\n"
-                        "Traceback:\n {}".format(str(type), str(value), "".join(traceback.format_tb(tb))))
+                        "Traceback:\n {}".format(str(error_type), str(value), "".join(traceback.format_tb(tb))))
 
 
 def setup_logging():
@@ -40,28 +40,28 @@ def setup_logging():
     setup_logger.addHandler(log_console_handler)
 
     setup_logger.setLevel(LOGGING_LEVEL)
+    sys.excepthook = log_exception_handler
 
 
-sys.excepthook = log_exception_handler
-setup_logging()
-logger = logging.getLogger("Main")
-
-
-def handle_startup():
+try:
     text = 'This is a test program. it demonstrates blah blah balh whatever.'
     parser = argparse.ArgumentParser(description=text)
     parser.add_argument("-r", "--rewrite", help="Use rewritten version", action="store_true")
     args = parser.parse_args()
 
-    if args.rewrite:
-        print("Rewrite currently unavailable")
-    else:
-        Pulseaudio_Loopback_Tool.setup_window()
+    setup_logging()
+    logger = logging.getLogger("Main")
 
-try:
-    # logger.info("Starting Python-Pulseaudio-Loopback-Tool")
-    # gui_logic.setup_window()
-    # TODO: Fix that stupid method name.
-    handle_startup()
+    if args.rewrite:
+        logger.info("Detected rewrite parameter. Rewrite is not finished, using this may break your sound until the"
+                    " next reboot.")
+        gui_logic.run_gui()
+        logger.info("Window appears to have been closed.")
+    else:
+        logger.info("Starting up deprecated version.")
+        Pulseaudio_Loopback_Tool.setup_window()
+        logger.info("Window appears to have been closed.")
 except KeyboardInterrupt:
+    setup_logging()
+    logger = logging.getLogger("Main")
     logger.info("Found KeyboardInterrupt, doing things.")
